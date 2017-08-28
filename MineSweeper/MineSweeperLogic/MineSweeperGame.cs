48932cf920988d63369e4ad6b16682889fe60b32
Filip Wall <filip.wall@lund.ntig.se>
2017-08-28 13:12:39 +0200

    Continued with ClickCoordinate

--- MineSweeper/MineSweeperLogic/MineSweeperGame.cs
+++ MineSweeper/MineSweeperLogic/MineSweeperGame.cs
@@ -21,6 +21,8 @@ namespace MineSweeperLogic
         public int SizeX { get; }
         public int SizeY { get; }
         public int NumberOfMines { get; }
+        public int NumberOfTiles { get; }
+        public int NumberOfOpenTiles;
         public GameState State { get; private set; }
         public MineSweeperGame Game;
 
@@ -36,18 +38,35 @@ namespace MineSweeperLogic
 
         public void ClickCoordinate()
         {
+            
             var positionOfPlayer = Game.GetCoordinate(PosX, PosY);
             if (positionOfPlayer.IsOpen == true)
             {
+                State = GameState.Playing;
                 return;
             }
             else
             {
                 positionOfPlayer.IsOpen = true;
+                NumberOfOpenTiles++;
+                //if (NumberOfTiles - NumberOfOpenTiles = NumberOfMines)
+                //{
+                //    State = GameState.Won;
+                //}
+                //else
+                //{
+                //    State = GameState.Playing;
+                //}
             }
+
             if (positionOfPlayer.HasMine == true)
             {
                 State = GameState.Lost;
+            }
+            else
+            {
+                positionOfPlayer.IsOpen = true;
+                NumberOfOpenTiles++;
             }
 
         }
